e7f55e1ac81f455216cc4e60468a97c3866dc676
KimKalura <raluca.deftu@yahoo.com>
2024-03-30 13:54:16 +0200

    board methods

--- src/main/java/com/spring/trelloclone/controller/BoardController.java
+++ src/main/java/com/spring/trelloclone/controller/BoardController.java
@@ -1,9 +1,19 @@
 package com.spring.trelloclone.controller;
 
+import com.spring.trelloclone.model.Board;
 import com.spring.trelloclone.service.BoardService;
+import com.spring.trelloclone.service.CsvExportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/board")
@@ -11,9 +21,29 @@ public class BoardController {
 
     private BoardService boardService;
 
+    private final CsvExportService csvExportService;
+
     @Autowired
-    public BoardController(BoardService boardService) {
+    public BoardController(BoardService boardService, CsvExportService csvExportService) {
         this.boardService = boardService;
+        this.csvExportService = csvExportService;
     }
+
+
+    @PostMapping("/create")
+    public Board createCategory(@RequestBody Board board) {
+        return boardService.addBoard(board);
+    }
+
+    @GetMapping("/myBoards")
+    public List<Board> getAllBoard() {
+        return boardService.getAllBoard();
+    }
+
+    @DeleteMapping("/delete/{boardId}")
+    public ResponseEntity<String> deleteBoard(@PathVariable Long boardId){
+        return boardService.deleteBoard(boardId);
+    }
+
 
 }
--- src/main/java/com/spring/trelloclone/service/BoardService.java
+++ src/main/java/com/spring/trelloclone/service/BoardService.java
@@ -1,12 +1,84 @@
 package com.spring.trelloclone.service;
 
+import com.spring.trelloclone.model.Board;
+import com.spring.trelloclone.model.Team;
+import com.spring.trelloclone.model.User;
 import com.spring.trelloclone.repository.BoardRepository;
+import com.spring.trelloclone.repository.ColRepository;
+import com.spring.trelloclone.repository.TeamRepository;
+import com.spring.trelloclone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class BoardService {
 
     private BoardRepository boardRepository;
+    private TeamRepository teamRepository;
+    private ColRepository colRepository;
+    private UserRepository userRepository;
 
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository, TeamRepository teamRepository, ColRepository colRepository, UserRepository userRepository) {
+        this.boardRepository = boardRepository;
+        this.teamRepository = teamRepository;
+        this.colRepository = colRepository;
+        this.userRepository = userRepository;
+    }
+
+    //Creez un nou board pentru o echipa (admin, team_leader)
+    public Board addBoard(Board board) {
+        Board foundBoard = boardRepository.findBoardByTitle(board.getTitle());//findById(board.getId());//
+        if (foundBoard == null) {
+            return boardRepository.save(board);
+        } else {
+            throw new ResponseStatusException(HttpStatus.CREATED, "board already exists");
+        }
+    }
+
+    //>>Vad toate board-urile din care fac parte(adica board-urile din echipele in care sunt membru) (admin, team_leader, team_member)
+    public List<Board> getAllBoard() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User foundUser = userRepository.findUserByUsername(userDetails.getUsername()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found"));
+        Set<Team> teams = foundUser.getTeam();
+        List<Board> allBoards = new ArrayList<>();
+        for (Team team : teams) {
+            allBoards.addAll(team.getBoardList());
+        }
+        return allBoards;
+    }
+
+
+    //>>Generez un raport in format CSV pentru un anumit board (admin, team_leader)
+    //Mergem prin fiecare coloana din board
+    //Numaram cate task-uri sunt in fiecare coloana din board
+    //Am putea sa generam un map in care cheia e numele coloanei si valoarea e numarul de task-uri din coloana
+    //De vazut daca din map-ul generat putem scrie in CSV
+
+    /*public CsvExportService csvBoard(){
+
+    }*/
+
+
+    //Sterg un anumit board (admin, team_leader)
+    public ResponseEntity<String> deleteBoard(Long boardId){ //f
+        Board foundBoard = boardRepository.findById(boardId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Board was not found"));
+        boardRepository.delete(foundBoard);
+
+        String responseMessage = "Board with id " + boardId + " has been successfully deleted.";
+        return ResponseEntity.ok(responseMessage);
+    }
 
 }
